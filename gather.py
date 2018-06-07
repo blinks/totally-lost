110da114f20a143aab81513e47c7043d0aa83e01
Adam Blinkinsop <blinks@acm.org>
2018-06-07 08:57:27 -0700

    Several quality of life changes.
    - Use AllCards instead of AllSets to automatically de-dupe.
    - Log various points in the process to understand where the gather script is.
    - Migrate card -> doc translation to schema.py to keep the schema in one place.
    - Set up the --source flag to enable defaults.

--- gather.py
+++ gather.py
@@ -6,14 +6,15 @@ import requests
 import zipfile
 import schema
 
-URL = 'https://mtgjson.com/json/AllSets.json.zip'
-LOCAL = 'AllSets.json'
+URL = 'https://mtgjson.com/json/AllCards.json.zip'
+LOCAL = 'AllCards.json'
 
 def main(source):
     # Retrieve or open the file of all set data.
     raw = None
     if source.startswith('http'):
         # TODO: Cache this locally, use a timestamp check for staleness.
+        print "Downloading card data."
         raw = io.BytesIO(requests.get(source).content)
     else:
         raw = open(source)
@@ -21,27 +22,25 @@ def main(source):
     # Unzip that file if necessary.
     jsonfile = raw
     if source.endswith('.zip'):
-        jsonfile = zipfile.ZipFile(raw).open('AllSets.json')
+        print "Decompressing card data."
+        jsonfile = zipfile.ZipFile(raw).open(LOCAL)
 
     # Index it.
+    print "Indexing card data."
     index(json.load(jsonfile))
 
 def index(data):
     """See https://mtgjson.com/documentation.html for json format."""
     ix = schema.create_index() # blow the old one away.
     writer = ix.writer()
-    for code in data:
-        for card in data[code]['cards']:
-            writer.add_document(
-                    name=card['name'],
-                    card_type=card.get('type', u''),
-                    rules_text=card.get('text', u''))
+    for card in data.values():
+        writer.add_document(**schema.convert(card))
     writer.commit() # TODO: optimize=True
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     # As arguments are added here, pass them to main as keyword args.
-    parser.add_argument('source', metavar='URL', type=str, default=URL)
+    parser.add_argument('--source', type=str, default=URL)
     args = parser.parse_args()
     main(args.source)
--- schema.py
+++ schema.py
@@ -1,15 +1,15 @@
 import os.path
 from whoosh.fields import SchemaClass, TEXT, KEYWORD, ID, STORED
-from whoosh.qparser import QueryParser
+from whoosh.qparser import MultifieldParser
 from whoosh import index
 
 class Card(SchemaClass):
     name = TEXT(stored=True)
-    rules_text = TEXT(stored=True)
-    card_type = KEYWORD(stored=True)
+    type = KEYWORD(stored=True)
+    text = TEXT(stored=True)
 
 _schema = Card()
-_qp = QueryParser('rules_text', schema=_schema)
+_qp = MultifieldParser(['name', 'type', 'text'], schema=_schema)
 
 def create_index(path='ix'):
     if not os.path.exists(path):
@@ -18,6 +18,14 @@ def create_index(path='ix'):
 
 def open_index(path='ix'):
     return index.open_dir(path)
+
+def convert(card):
+    """Convert from https://mtgjson.com/documentation.html#cards to a document."""
+    return {
+        'name': u' // '.join(card.get('names', [card['name']])),
+        'type': card.get('type', u''),
+        'text': card.get('text', u''),
+    }
 
 def parse(query):
     return _qp.parse(query)
