a15e90e3529934f0e9bcab397c90a98b1e444bd3
Adam Blinkinsop <blinks@acm.org>
2019-10-01 10:14:09 -0700

    Upgrade to Python3.
    Also bring requirements up to modern speed.

--- gather.py
+++ gather.py
@@ -1,26 +1,29 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Pull cards down from mtgjson.com and re-index.
 import io
 import json
+import os.path
+import os
 import requests
-import zipfile
 import schema
+import zipfile
 
-URL = 'https://mtgjson.com/json/AllCards-x.json.zip'
-LOCAL = 'AllCards-x.json'
+URL = 'https://mtgjson.com/json/AllCards.json.zip'
+LOCAL = 'AllCards.json'
 
 def main():
     # Retrieve or open the file of all set data.
     # TODO: Cache this locally, use a timestamp check for staleness.
-    print "Downloading card data."
+    print("Downloading card data.")
     raw = io.BytesIO(requests.get(URL).content)
 
     # Unzip that file if necessary.
-    print "Decompressing card data."
+    print("Decompressing card data.")
     jsonfile = zipfile.ZipFile(raw).open(LOCAL)
 
     # Index it.
-    print "Indexing card data."
+    print("Indexing card data.")
+    os.path.isdir(schema.ROOT) or os.makedirs(schema.ROOT)
     index(json.load(jsonfile))
 
 def index(data):
--- schema.py
+++ schema.py
@@ -4,6 +4,8 @@ from whoosh.fields import SchemaClass, TEXT, KEYWORD, ID, STORED, NUMERIC
 from whoosh.filedb.filestore import FileStorage
 from whoosh import analysis, index, qparser, sorting
 
+ROOT = 'ix'
+
 text_analyzer = analysis.RegexTokenizer() |\
         analysis.SubstitutionFilter(r'{(\w+?)}', r'\1') |\
         analysis.StemFilter()
@@ -12,7 +14,8 @@ class Card(SchemaClass):
     name = TEXT(stored=True)
     layout = KEYWORD()
     cost = STORED()
-    cmc = NUMERIC(numtype=int)
+    cmc = STORED()
+    ncmc = NUMERIC(numtype=int)
     colors = KEYWORD(lowercase=True, scorable=True)
     type = KEYWORD(lowercase=True, scorable=True, stored=True)
     text = TEXT(analyzer=text_analyzer, stored=True)
@@ -20,29 +23,33 @@ class Card(SchemaClass):
     npower = NUMERIC(numtype=int)
     toughness = STORED()
     ntoughness = NUMERIC(numtype=int)
-    loyalty = NUMERIC()
+    loyalty = STORED()
+    nloyalty = NUMERIC()
 
 _schema = Card()
 
-def create_index(storage=FileStorage('ix')):
+def create_index(storage=FileStorage(ROOT)):
     return storage.create_index(_schema)
 
-def open_index(storage=FileStorage('ix')):
+def open_index(storage=FileStorage(ROOT)):
     return storage.open_index()
 
 def convert(card):
     """Convert from https://mtgjson.com/documentation.html#cards to a document."""
+
     document = {
         'name': u' // '.join(card.get('names', [card['name']])),
         'layout': card.get('layout', u''),
         'cost': card.get('manaCost', u''),
-        'cmc': int(card.get('cmc', u'0')),
         'colors': u' '.join(card.get('colors', [])),
         'type': card.get('type', u''),
         'text': card.get('text', u''),
     }
 
-    non = re.compile(r'[^-+0-9]')
+    non = re.compile(r'[^-+0-9.]')
+    if 'cmc' in card:
+        document['cmc'] = card['cmc']
+        document['ncmc'] = eval(non.sub('0', card['cmc']))
     if 'power' in card:
         document['power'] = card['power']
         document['npower'] = eval(non.sub('0', card['power']))
@@ -51,13 +58,14 @@ def convert(card):
         document['ntoughness'] = eval(non.sub('0', card['toughness']))
     if 'loyalty' in card:
         document['loyalty'] = card['loyalty']
+        document['nloyalty'] = eval(non.sub('0', card['loyalty']))
 
     # TODO: "legalities" converted to a useful schema.
 
     return document
 
 # Configure the QueryParser.
-_qp = qparser.MultifieldParser(['name', 'type', 'cmc', 'text'], schema=_schema)
+_qp = qparser.MultifieldParser(['name', 'type', 'ncmc', 'text'], schema=_schema)
 
 def parse(query):
     return _qp.parse(query)
@@ -65,5 +73,5 @@ def parse(query):
 def search(searcher, query):
     """Returns a tuple: (results, query)"""
     q = parse(query)
-    results = searcher.search(q, groupedby='cmc', maptype=sorting.Count)
+    results = searcher.search(q, groupedby='ncmc', maptype=sorting.Count)
     return results, query
